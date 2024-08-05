0f264c51e22357637f1b11d645e0ab3e036f4f72
rev-kohei.yaeo <yaeok@revolveok.co.jp>
2024-08-05 17:15:45 +0900

    サイドバーの更新

--- components/target/detail/SideBar.tsx
+++ components/target/detail/SideBar.tsx
@@ -2,33 +2,41 @@ import React from 'react'
 
 const Sidebar: React.FC = () => {
   return (
-    <aside className='w-1/5 h-screen z-25 sticky bg-gray-200 block left-0 top-0'>
-      <div className='px-5 py-24'>
+    <div className='w-1/5 h-screen z-25 sticky bg-gray-200 block left-0 top-0'>
+      <div className='px-5 pt-24'>
         <h3 className='font-bold text-lg'>メニュー</h3>
         <ul className='mt-4'>
-          <li className='mb-2'>
-            <a href='#' className='block text-gray-900 hover:text-blue-500'>
-              ホーム
-            </a>
+          <li
+            className='mb-2 cursor-pointer'
+            onClick={() => console.log('onClick')}
+          >
+            <p className='block text-gray-900 hover:text-blue-500'>ホーム</p>
           </li>
-          <li className='mb-2'>
-            <a href='#' className='block text-gray-900 hover:text-blue-500'>
+          <li
+            className='mb-2 cursor-pointer'
+            onClick={() => console.log('onClick')}
+          >
+            <p className='block text-gray-900 hover:text-blue-500'>
               プロファイル
-            </a>
+            </p>
           </li>
-          <li className='mb-2'>
-            <a href='#' className='block text-gray-900 hover:text-blue-500'>
-              設定
-            </a>
+          <li
+            className='mb-2 cursor-pointer'
+            onClick={() => console.log('onClick')}
+          >
+            <p className='block text-gray-900 hover:text-blue-500'>設定</p>
           </li>
-          <li className='mb-2'>
-            <a href='#' className='block text-gray-900 hover:text-blue-500'>
+          <li
+            className='mb-2 cursor-pointer'
+            onClick={() => console.log('onClick')}
+          >
+            <p className='block text-gray-900 hover:text-blue-500'>
               ログアウト
-            </a>
+            </p>
           </li>
         </ul>
       </div>
-    </aside>
+    </div>
   )
 }
 
